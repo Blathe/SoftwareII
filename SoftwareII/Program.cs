8d1b17ee84912f2be407c57af61f224c20dc898c
Scott <scottpeters2281@gmail.com>
2022-10-29 05:49:55 +0000

    Added DB Connection service.

--- SoftwareII/Program.cs
+++ SoftwareII/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SoftwareII.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,10 @@ namespace SoftwareII
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            DBConnectionService.StartConnection();
             Application.Run(new Form1());
+            DBConnectionService.CloseConnection();
         }
     }
 }
--- SoftwareII/Services/DBConnectionService.cs
+++ SoftwareII/Services/DBConnectionService.cs
@@ -1,17 +1,40 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SoftwareII.Services
 {
     class DBConnectionService
     {
-
-        public DBConnectionService()
+        public static MySqlConnection connection { get; set; }
+        public static void StartConnection()
         {
+            var connectionString = ConfigurationManager.ConnectionStrings["localdb"].ConnectionString;
+            try
+            {
+                connection = new MySqlConnection(connectionString);
 
+                connection.Open();
+                MessageBox.Show("Connection is Open!");
+
+            } catch (MySqlException ex)
+            {
+                //TODO
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public static void CloseConnection()
+        {
+            if (connection != null)
+            {
+                connection.Close();
+            }
         }
     }
 }
